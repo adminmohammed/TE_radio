f206ae1b42697da28c8e74215deffdb15e20ef63
adminmohammed <105809983+adminmohammed@users.noreply.github.com>
2025-08-07 02:53:10 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,58 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'VRP Radio System - FastScripts'
+description 'Advanced VRP Radio System with Permissions'
+version 'release-1.3.1-vrp'
+lua54 'yes'
+
+name 'TE_Radio'
+version 'v1.2.1'
+author 'ARMANDO'
+
+dependencies {
+    'vrp',
+    'oxmysql'
+}
+
+escrow_ignore {
+
+    "shared/*.lua",
+}
+
+shared_scripts {
+    '@vrp/lib/utils.lua',
+    'shared/sh_config.lua'
+}
+
+
+client_scripts {
+    'client/*.lua'
+}
+
+
+server_scripts {
+    '@vrp/lib/utils.lua',
+    '@oxmysql/lib/MySQL.lua',
+    'shared/sv_config.lua',
+    'server/*.lua'
+}
+
+ui_page 'html/index.html'
+
+
+files {
+    "html/index.html",
+    "html/js/*.js",
+    "html/css/*.css",
+    "html/img/*.png",
+    "html/img/*.jpg",
+    "html/fonts/*.ttf",
+    "html/sound/*.mp3",
+    "html/sound/*.ogg",
+    "html/sound/*.wav",
+    "html/img/custom/*.png",
+    "html/img/custom/*.jpg",
+    "html/js/node_modules/animejs/lib/*.js"
+}
+
